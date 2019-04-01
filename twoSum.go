8a7c735dc6276b124d3de61bde6147c2cd05dbdf
songyichao <songyichao74@gmail.com>
2019-04-01 21:28:34 +0800

    two_sum

---
+++ twoSum.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		l := target - nums[i]
+		v, e := m[l]
+		if e {
+			return []int{i, v}
+		}
+		m[nums[i]] = i
+	}
+	return []int{}
+}
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	fmt.Println(nums)
+	res := twoSum(nums, 9)
+	fmt.Println(res)
+}
