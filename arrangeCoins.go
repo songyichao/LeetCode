d27b2df38fc1efa0f50881cdfbab289af063d9f4
songyichao <songyichao74@gmail.com>
2019-05-16 20:42:07 +0800

    arrangeCoins

--- arrangeCoins.go
+++ arrangeCoins.go
@@ -3,13 +3,37 @@ package main
 import "fmt"
 
 func arrangeCoins(n int) int {
-	//递减就完事了
-	res := 0
-	for i := 1; n >= i; i++ {
-		n -= i
-		res = i
+	//二分查找
+	if n == 0 {
+		return 0
 	}
-	return res
+	if n <0 {
+		return -1
+	}
+	left, right := 1, n
+
+	for left <= right {
+		mid := left + (right-left)/2
+		sum := mid * (1+mid)/2
+		if sum > n {
+			right = mid-1
+		}
+		if sum < n {
+			left = mid +1
+		}
+		if sum == n {
+			return mid
+		}
+	}
+
+	return left-1
+	//递减就完事了
+	//res := 0
+	//for i := 1; n >= i; i++ {
+	//	n -= i
+	//	res = i
+	//}
+	//return res
 }
 
 func main() {
