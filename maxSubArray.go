4cae25097e86dd8193f731cd611aeccd71a79080
songyichao <songyichao74@gmail.com>
2019-06-14 11:14:13 +0800

    maxSubArray

--- maxSubArray.go
+++ maxSubArray.go
@@ -6,19 +6,19 @@ import (
 )
 
 func maxSubArray(nums []int) int {
-	res := math.MaxInt64
+	res := math.MinInt64
 	sum := 0
 	for _, v := range nums {
-		sum = int(math.Max(float64(sum+v), float64(v)))
-		res = int(math.Max(float64(res), float64(sum)))
-	}
+	sum = int(math.Max(float64(sum+v), float64(v)))
+	res = int(math.Max(float64(res), float64(sum)))
+}
 
 	return res
 }
 
-func main() {
+	func main() {
 	nums := []int{
-		-2, 1, -3, 4, -1, 2, 1, -5, 4,
+		1,
 	}
 	fmt.Println(maxSubArray(nums))
 }
